f6b7e0e016636afbc50dbf085d0e04356f0cab7e
WENDYAKINYI <113832323+WENDYAKINYI@users.noreply.github.com>
2025-06-01 15:16:11 -0600

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,11 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # --- Model Loading ---
 @st.cache_resource
 def get_models():
-    return load_baseline_model()
+    try:
+        return load_baseline_model()
+    except Exception as e:
+        st.error("❌ Failed to load models from Hugging Face.")
+        st.stop()
 
 encoder, decoder, vocab = get_models()
 
