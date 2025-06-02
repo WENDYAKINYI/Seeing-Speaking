4b351266871acdd2e00ae9e2aa880effc9397096
WENDYAKINYI <113832323+WENDYAKINYI@users.noreply.github.com>
2025-06-01 20:45:11 -0600

    Update models.py

--- models.py
+++ models.py
@@ -8,14 +8,19 @@ class EncoderCNN(nn.Module):
     def __init__(self, embed_size):
         super(EncoderCNN, self).__init__()
         resnet = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
-        modules = list(resnet.children())[:-1]  # Remove final FC layer
+        modules = list(resnet.children())[:-1]  # remove FC layer
         self.resnet = nn.Sequential(*modules)
+        self.linear = nn.Linear(resnet.fc.in_features, embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
 
     def forward(self, images):
         with torch.no_grad():
             features = self.resnet(images)  # [B, 2048, 1, 1]
-            features = features.view(features.size(0), -1)  # Flatten to [B, 2048]
-        return features
+            features = features.view(features.size(0), -1)  # flatten
+        features = self.linear(features)
+        features = self.bn(features)
+        return features  # [B, embed_size]
+
 
 
 class DecoderRNN(nn.Module):
